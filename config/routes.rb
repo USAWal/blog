d3f3d1e3102eb092c71ea31691511012252adb97
Valeriy Utyaganov <mac-1@Mac-mini-mac.local>
2015-03-17 18:00:23 +0300

    Fixed #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  root 'articles#index'
+
   authenticate :user do
     mount Sidekiq::Web => '/sidekiq'
   end
@@ -11,9 +13,6 @@ Rails.application.routes.draw do
   get 'unsubscribe', to: 'welcome#unsubscribe', as: 'unsubscribe'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
